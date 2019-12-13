15f4fc9eafc8e86fb93ef06f432535fed3020316
Jonathan Grajkowski <jonathangrajkowski@Jonathans-MacBook-Pro.local>
2019-12-13 10:26:39 -0500

    little tester

--- Delegate_Protocol/TeamSelectionViewController.swift
+++ Delegate_Protocol/TeamSelectionViewController.swift
@@ -8,23 +8,27 @@
 
 import UIKit
 
+protocol TeamSelectionDelegate {
+    func didChooseTeam(image: UIImage, name: String)
+}
+
 class TeamSelectionViewController: UIViewController {
 
+    let image_marner = #imageLiteral(resourceName: "marner")
+    let image_pastrnak = #imageLiteral(resourceName: "pastrnak")
+    var delegate: TeamSelectionDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func bustonButtonTapped(_ sender: UIButton) {
+        delegate.didChooseTeam(image: image_pastrnak, name: "David Pastrnak")
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func torontoButtonTapped(_ sender: UIButton) {
+        delegate.didChooseTeam(image: image_marner, name: "Mitch Marner")
+        dismiss(animated: true, completion: nil)
+    }
 }
--- Delegate_Protocol/ViewController.swift
+++ Delegate_Protocol/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var teamImage: UIImageView!
+    @IBOutlet weak var playerName: UITextView!
+    
+    @IBAction func chooseTeamTapped(_ sender: Any) {
+        let VC = storyboard?.instantiateViewController(withIdentifier: "TeamSelectionViewController") as! TeamSelectionViewController
+        VC.delegate = self
+        present(VC, animated: true,completion: nil)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+}
+    extension ViewController: TeamSelectionDelegate{
+        func didChooseTeam(image: UIImage, name: String) {
+            teamImage.image = image
+            playerName.text = name
+        }
     }
 
-
-}
 
